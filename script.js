90bfb5c47460b16b8e73ce18af05aa4005864589
sojip <sojipquantin@gmail.com>
2021-04-01 10:19:42 +0100

    grid UI

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
-
-let gridNumber = 16;
 let container = document.querySelector(".container");
-let width = container.offsetWidth;
-let height = container.offsetHeight;
+let width = container.offsetWidth - 2;
+let height = container.offsetHeight - 2;
+createGrid(16);
 
-console.log(width)
+let divs = document.querySelectorAll('.square');
+
+window.addEventListener('resize', () => {
+    changeSquareDimension(16)
+});
+
+divs.forEach( div => {
+    div.addEventListener("mouseover", () => {
+        div.style.background = "black";
+    })
+} )
+
+let button = document.querySelector('#createGrid');
+button.addEventListener('click', () => {
+    let gridSize = document.getElementById("gridSize").value;
+    createGrid(gridSize);
+    changeSquareDimension(gridSize);
+})
 
 
-for(let i=0; i < (gridNumber*gridNumber); i++) {
-    let div = document.createElement("div");
-    div.classList.add("carreau");
-    div.style.minWidth = String(width/(gridNumber+1)) + "px";
-    div.style.minHeight = String(height/(gridNumber+1)) + "px";
-    container.appendChild(div);
-    console.log(div.style.width)
+// function changeSquareDimension(gridNumber) {
+    
+//     width = container.offsetWidth;
+//     height = container.offsetHeight;
+//     divs.forEach( div => {
+//         div.style.minWidth = String(((width)/gridNumber)-4) + "px";
+//         div.style.minHeight = String(((height)/gridNumber)-4) + "px";
+//     } )
+// }
+
+function createGrid(gridNumber) {
+    width = container.offsetWidth;
+    console.log(width)
+    height = container.offsetHeight;
+    for(let i=0; i < (gridNumber*gridNumber); i++) {
+        let div = document.createElement("div");
+        div.classList.add("square");
+        // div.style.minWidth = String(((width)/gridNumber)) + "px";
+        // console.log(div.style.minWidth)
+        // div.style.minHeight = String(((height)/gridNumber)) + "px";
+        container.appendChild(div);
+        // console.log(div.offsetWidth)
+    }
 }
